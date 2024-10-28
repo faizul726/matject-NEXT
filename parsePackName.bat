bd6bafdb4d8850fb5f3f2747f26c3632bf5d3075
faizul726 <162413089+faizul726@users.noreply.github.com>
2024-10-28 12:35:18 +0600

    fix parser again

--- parsePackName.bat
+++ parsePackName.bat
@@ -1,14 +1,12 @@
 for /f "delims=" %%i in ('jq -r ".[0].pack_id" "%gamelocation%\minecraftpe\global_resource_packs.json"') do set "uuid=%%i"
+echo %uuid%
 for /d %%D in ("%gamelocation%\resource_packs\*") do (
     if exist "%%D\manifest.json" (
         for /f "delims=" %%i in ('jq -r ".header.name" "%%D\manifest.json"') do (
             set "packName=%%i"
             for /f "delims=" %%j in ('jq -r ".header.uuid" "%%D\manifest.json"') do (
                 set "packuuid=%%j"
-                if "!packuuid!"=="%uuid%" (
-                    echo [92mNewly activated first pack is: !packName![0m
-                    goto monitor
-                )
+                if !packuuid! equ %uuid% echo [92mNewly activated first pack is: !packName![0m
             )
         )
     )
