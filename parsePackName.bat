fcf0b3f700c85be6a12d780ad61a2ede9f5bd171
faizul726 <162413089+faizul726@users.noreply.github.com>
2024-10-29 10:51:59 +0600

    add:subpack parser

--- parsePackName.bat
+++ parsePackName.bat
@@ -6,10 +6,15 @@ for /d %%D in ("%gamelocation%\resource_packs\*") do (
             for /f "delims=" %%j in ('jq -r ".header.uuid" "%%D\manifest.json"') do (
                 set "packuuid=%%j"
                 if !packuuid! equ %uuid% (
-                    echo [97m^> Newly activated first pack is:  [92m!packName![0m
-                    goto:EOF
+                    echo [97m^> First activated pack: [92m!packName![0m
+                    for /f "delims=" %%i in ('jq ".[0] | has(\"subpack\")" "%gamelocation%\minecraftpe\global_resource_packs.json"') do (
+                        set "hasSubpack=%%i"
+                        echo [97m^> hasSubpack: [92m!hasSubpack![0m
+                        if !hasSubpack! equ true (call parseSubpack) else (goto:EOF)
+                    )
                 )
             )
         )
     )
 )
+if !jqoutput! equ true call parseSubpack
---
+++ parseSubpack.bat
@@ -0,0 +1,3 @@
+for /f "delims=" %%i in ('jq -r ".[0].subpack" "%gamelocation%\minecraftpe\global_resource_packs.json"') do set "subpackName=%%i"
+echo [97m^> Subpack name: [92m!subpackName![0m
+goto:EOF
